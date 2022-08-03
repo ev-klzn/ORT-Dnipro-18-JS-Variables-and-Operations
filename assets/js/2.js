44de2de7743b48e2f0e7c087dbc86bdf3d3285ec
Ev-klzn <zhekan89@gmail.com>
2022-08-03 16:29:32 +0300

    2

---
+++ assets/js/2.js
@@ -0,0 +1,6 @@
+let a = +prompt(`длина ребра куба, см.`);
+console.log(`длина ребра куба, см ${a}.`);
+let V = a * a * a;
+console.log(`обьем куба, см ${V}.`);
+let SBok = 4 * (a * a);
+console.log(`Площадь боковой поверхности куба, ${SBok}`);
