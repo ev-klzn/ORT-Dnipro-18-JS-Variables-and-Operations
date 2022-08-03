39aa2704f7f277fb9d22b01c7024a17929380a63
ev-klzn <zhekan89@gmail.com>
2022-08-03 20:14:32 +0300

    3

--- assets/js/3.js
+++ assets/js/3.js
@@ -1,7 +1,7 @@
 let r = +prompt('Радиус окружности, см');
 console.log(`Радиус окружности ${r}см.`);
-const P = 3.14;
-let L = r * P;
-console.log(`Длина окружнсти ${L}см.`);
-let S = P * (r * r);
+const pi = 3.14;
+let P = 2 * pi * r;
+console.log(`Длина окружнсти ${P}см.`);
+let S = pi * (r * r);
 console.log(`Площадь круга ${S}см2`);
