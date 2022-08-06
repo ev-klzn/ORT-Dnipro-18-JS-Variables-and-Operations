5c3ebce967c8e1f1b32585eadcc19cbbe031bdd3
Ev-klzn <zhekan89@gmail.com>
2022-08-06 17:58:10 +0300

    2

--- assets/js/1.js
+++ assets/js/1.js
@@ -2,3 +2,4 @@ let a = +prompt('длина стороны квадрата, см.');
 console.log(`Длина стороны квадрата: ${a} см.`);
 let perimetr = a * 4;
 console.log(`Периметр квадрата: ${perimetr} см.`);
+alert(`Периметр квадрата: ${perimetr}`);
